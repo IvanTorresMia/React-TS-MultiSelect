38f25f0d9fd169a81a4935cfab22e24333d62bf8
IvanTorresMia <ivantorresmia27@gmail.com>
2023-04-20 13:04:01 -0700

    multi select implement

--- multi-select-ts/src/Select.tsx
+++ multi-select-ts/src/Select.tsx
@@ -1,39 +1,78 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./select.module.css";
 
-type SelectOption = {
+export type SelectOption = {
   label: String;
-  value: number;
+  value: number | string;
 };
 
-type SelectProps = {
-  options: SelectOption[];
+type MultipleSelectProps = {
+  multiple: true;
+  value: SelectOption[];
+  onChange: (value: SelectOption[]) => void;
+};
+
+type SingleSelectProps = {
+  multiple?: false;
   value?: SelectOption;
   onChange: (value: SelectOption | undefined) => void;
 };
 
-export function Select({ value, onChange, options }: SelectProps) {
+type SelectProps = {
+  options: SelectOption[];
+} & (SingleSelectProps | MultipleSelectProps);
+
+export function Select({ multiple, value, onChange, options }: SelectProps) {
   const [isOpen, setIsOpen] = useState(false);
+  const [highlightedIndex, setHighlightedIndex] = useState<number>(0);
   function clearOptions() {
-    onChange(undefined);
+    multiple ? onChange([]) : onChange(undefined);
   }
 
   function selectOption(option: SelectOption) {
-    onChange(option);
+    if (multiple) {
+      if (value.includes(option)) {
+        onChange(value.filter((o) => o !== option));
+      } else {
+        onChange([...value, option]);
+      }
+    } else {
+      if (option !== value) onChange(option);
+    }
   }
 
-function isOptionSelected(option: SelectOption) {
-    return option === value;
-}
+  function isOptionSelected(option: SelectOption) {
+    return multiple ? value.includes(option) : option === value;
+  }
+
+  useEffect(() => {
+    if (isOpen) setHighlightedIndex(0);
+  }, [isOpen]);
 
   return (
     <div
-      onBlur={() => setIsOpen(false)} 
+      onBlur={() => setIsOpen(false)}
       onClick={() => setIsOpen((prev) => !prev)}
       tabIndex={0}
       className={styles.container}
     >
-      <span className={styles.value}>{value?.label}</span>
+      <span className={styles.value}>
+        {multiple
+          ? value.map((v) => (
+              <button
+                key={v.value}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  selectOption(v);
+                }}
+                className={styles["option-badge"]}
+              >
+                {v.label}
+                <span className={styles["remove-btn"]}>&times;</span>
+              </button>
+            ))
+          : value?.label}
+      </span>
       <button
         onClick={(e) => {
           e.stopPropagation();
@@ -46,15 +85,20 @@ function isOptionSelected(option: SelectOption) {
       <div className={styles.divider}></div>
       <div className={styles.carrot}></div>
       <ul className={`${styles.options} ${isOpen ? styles.show : ""}`}>
-        {options.map((option) => (
+        {options.map((option, index) => (
           <li
             onClick={(e) => {
               e.stopPropagation();
               selectOption(option);
               setIsOpen(false);
             }}
+            onMouseEnter={() => setHighlightedIndex(index)}
             key={option.value}
-            className={`${styles.option} ${isOptionSelected(option) ? styles.selected: ""}`}
+            className={`${styles.option} ${
+              isOptionSelected(option) ? styles.selected : ""
+            }
+            ${index === highlightedIndex ? styles.highlighted : ""}
+            `}
           >
             {option.label}
           </li>
